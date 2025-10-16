cac80c01891a341646071da3868589b6a2b58f1d
joelchristian7 <djoelchristian777@gmail.com>
2025-10-17 01:38:03 +0530

    Daily News Summary Implemented

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,15 +1,16 @@
-'use client'
-import React from 'react'
-import {useForm} from "react-hook-form";
-import InputField from "@/components/forms/InputField";
-import {Button} from "@/components/ui/button";
-import FooterLink from "@/components/forms/FooterLink";
-import {signInWithEmail} from "@/lib/actions/auth.actions";
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import InputField from '@/components/forms/InputField';
+import FooterLink from '@/components/forms/FooterLink';
+import {signInWithEmail, signUpWithEmail} from "@/lib/actions/auth.actions";
 import {toast} from "sonner";
+import {signInEmail} from "better-auth/api";
 import {useRouter} from "next/navigation";
 
 const SignIn = () => {
-    const router = useRouter();
+    const router = useRouter()
     const {
         register,
         handleSubmit,
@@ -19,8 +20,8 @@ const SignIn = () => {
             email: '',
             password: '',
         },
-        mode: 'onBlur'
-    },);
+        mode: 'onBlur',
+    });
 
     const onSubmit = async (data: SignInFormData) => {
         try {
@@ -28,43 +29,43 @@ const SignIn = () => {
             if(result.success) router.push('/');
         } catch (e) {
             console.error(e);
-            toast.error('Sign In failed', {
-                description: e instanceof Error ? e.message : 'Failed to SignIn'
+            toast.error('Sign in failed', {
+                description: e instanceof Error ? e.message : 'Failed to sign in.'
             })
         }
     }
 
     return (
         <>
-            <h1 className="form-title">Welcome Back to Stock-Sense</h1>
+            <h1 className="form-title">Welcome back</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="johnwick@gmail.com"
+                    placeholder="contact@jsmastery.com"
                     register={register}
                     error={errors.email}
-                    validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/, message: 'Email is required' }}
+                    validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/ }}
                 />
 
                 <InputField
                     name="password"
                     label="Password"
-                    placeholder="Enter a strong password"
+                    placeholder="Enter your password"
                     type="password"
                     register={register}
                     error={errors.password}
-                    validation={{ required: 'Password is required', minLength: 8  }}
+                    validation={{ required: 'Password is required', minLength: 8 }}
                 />
 
                 <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
-                    {isSubmitting ? 'Signing In' : 'Log In'}
+                    {isSubmitting ? 'Signing In' : 'Sign In'}
                 </Button>
 
-                <FooterLink text="Don't have an Account?" linkText="Sign up" href="/sign-up" />
+                <FooterLink text="Don't have an account?" linkText="Create an account" href="/sign-up" />
             </form>
         </>
-    )
-}
-export default SignIn
+    );
+};
+export default SignIn;
